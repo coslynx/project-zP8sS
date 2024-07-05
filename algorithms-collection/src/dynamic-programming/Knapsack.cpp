497cfb87e9ccc38c7fdf41940eed12409a9192be
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-05 12:04:08 +0530

    Generated: `algorithms-collection/src/dynamic-programming/Knapsack.cpp`

---
+++ algorithms-collection/src/dynamic-programming/Knapsack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int knapsack(int W, vector<int>& weights, vector<int>& values, int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 1; w <= W; w++) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][W];
+}
