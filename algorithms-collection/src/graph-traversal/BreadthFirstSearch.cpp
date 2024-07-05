dec7784b3e868b36f7ca34b07a7ff2774a9926b3
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-05 12:04:01 +0530

    Generated: `algorithms-collection/src/graph-traversal/BreadthFirstSearch.cpp`

---
+++ algorithms-collection/src/graph-traversal/BreadthFirstSearch.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Graph {
+private:
+    int V;
+    vector<vector<int>> adj;
+
+public:
+    Graph(int vertices) {
+        V = vertices;
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+
+    void BFS(int start) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        while (!q.empty()) {
+            int curr = q.front();
+            cout << curr << " ";
+            q.pop();
+
+            for (int neighbor : adj[curr]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Breadth First Traversal starting from vertex 2: ";
+    g.BFS(2);
+
+    return 0;
+}
