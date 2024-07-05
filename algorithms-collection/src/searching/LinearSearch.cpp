57ccf28d7de2c538525a986e96126522351820bb
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-05 12:03:48 +0530

    Generated: `algorithms-collection/src/searching/LinearSearch.cpp`

---
+++ algorithms-collection/src/searching/LinearSearch.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+int linearSearch(std::vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
